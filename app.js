a7996f931389cf37e8d6d98a03ffa68bf5b48a9f
manish <manishmaffia100200300@gmail.com>
2022-09-19 11:20:40 +0530

    upgrading

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const https = require('https');
 const _ = require("lodash");
 let posts = [];
 
@@ -27,23 +28,26 @@ app.get("/", function(req, res) {
 
 });
 
-app.get("https://daily-journal12.herokuapp.com/about", function(req, res) {
+app.get("/about", function(req, res) {
   res.render("About", {
-    AboutContent: aboutContent
+    AboutContent:aboutContent
   });
 });
 
-app.get("https://daily-journal12.herokuapp.com/contact", function(req, res) {
+app.get("/contact",function(req, res) {
   res.render("Contact", {
     contactContent: contactContent
   });
 });
+app.post("/about", function (req, res) {
+    res.redirect("About");
+});
 
-app.get("https://daily-journal12.herokuapp.com/compose", function(req, res) {
+app.get("/compose",function(req, res) {
   res.render("Compose");
 });
 
-app.post("https://daily-journal12.herokuapp.com/compose", function(req, res) {
+app.post("/compose", function(req, res) {
   const post = {
     title: req.body.postTitle,
     content: req.body.postBody
